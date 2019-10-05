d5614a1f056ef5deb92ffe989856e15471a00063
mehulpratapsingh <56200474+mehulpratapsingh@users.noreply.github.com>
2019-10-05 23:34:14 +0530

    ans 5

---
+++ ReplaceString.java
@@ -0,0 +1,18 @@
+package Solution5;
+
+import java.util.Scanner;
+
+public class ReplaceString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:-");
+        String s1 = sc.next();
+        System.out.println("Enter the Substring");
+
+        String substring1 = sc.next();
+        System.out.println("enter the value to replaced with Substring : ");
+        String s3 = sc.next();
+        String ansString = s1.replace(substring1,s3);
+        System.out.println(ansString);
+    }
+}
